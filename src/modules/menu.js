04a32383189bf6d072d61a3b5a912b95d75086f3
Bjorn <bjorneng3@gmail.com>
2023-02-03 16:36:59 -0500

    Menu tab complete

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -4,52 +4,36 @@ const menuBack = document.createElement('div');
 menuBack.setAttribute('id','menuBack');
 
 
-function createMenu() {
+function createPizzaMenu() {
     const menuBack = document.getElementById("menuBack");
+    const menuItems = ['Cheese', 'Pepperoni', 'Vegetable', 'Meat Lovers', 'Hawaiian', 'Margherita'];
+    const menuPrices = ['$5.00', '$6.00', '$6.50', '$7.50', '$7.00', '$6.00'];
+    const menuPics = ["../dist/imgs/cheese-pizza.jpeg", "../dist/imgs/pepperoni-pizza.jpeg",
+    "../dist/imgs/vegetable-pizza.png", "../dist/imgs/meat-lovers-pizza.jpeg", "../dist/imgs/hawaiian-pizza.jpeg", "../dist/imgs/margherita-pizza.png"]
   
-    for (let i = 0; i < 6; i++) {
-      const div = document.createElement("div");
-      menuBack.appendChild(div);
-      div.classList.add('pizza');
-    }
-}
-
-function changeMenu() {
-    const menuItems = document.querySelectorAll(".pizza");
-
     for (let i = 0; i < menuItems.length; i++) {
-        menuItems[0].setAttribute('id','cheese');
-        menuItems[0]
+      const menuItem = document.createElement("div");
+      menuItem.classList.add("menu-item");
+      const itemImg = document.createElement("img");
+      itemImg.src = menuPics[i];
+      const itemTitle = document.createElement("h2");
+      itemTitle.innerHTML = menuItems[i];
+      //itemTitle.setAttribute('id', menuItems[i])
+      const itemPrice = document.createElement("h2");
+      itemPrice.innerHTML = menuPrices[i];
+      menuItem.appendChild(itemImg);
+      menuItem.appendChild(itemTitle);
+      menuItem.appendChild(itemPrice);
+      menuBack.appendChild(menuItem);
     }
-}
-
-function menuDetails () {
-    const cheese = document.getElementById('cheese');
-    const cheeseImg = document.createElement('div');
-    cheeseImg.setAttribute('id','cheeseImg');
-    const cheeseContainer = document.createElement('div');
-    cheeseContainer.setAttribute('id','cheeseContainer');
-    const cheeseTitle = document.createElement('h2');
-    cheeseTitle.setAttribute('id','cheeseTitle');
-    cheeseTitle.innerHTML = 'Cheese Pizza';
-    const cheesePrice = document.createElement('h2');
-    cheesePrice.setAttribute('id','cheesePrice');
-    cheesePrice.innerHTML = '$5.00';
-    cheese.appendChild(cheeseContainer);
-    cheeseContainer.appendChild(cheeseImg);
-    cheeseContainer.appendChild(cheeseTitle);
-    cheeseContainer.appendChild(cheesePrice);
-
-}
+  }
 
 function loadMenu() {
     document.getElementById("title").remove();
     document.getElementById("slogan").remove();
     content.appendChild(backContainer);
     backContainer.appendChild(menuBack);
-    createMenu();
-    changeMenu();
-    menuDetails();
+    createPizzaMenu();
 }
 
 export default loadMenu;
